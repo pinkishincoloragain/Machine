2ea572a7f805cf388c2407a168702957c8b1c0ca
pinkishincoloragain <smb1103@gmail.com>
2021-07-07 17:15:45 +0000

    added time stampo

--- detection/face_detection.py
+++ detection/face_detection.py
@@ -2,10 +2,12 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import time
 
 if __name__ == "__main__":
     # print(cv2.__version__)
-    image1 = cv2.imread("data/tak.jpg")
+    temp = time.time()
+    image1 = cv2.imread("data/small_data.png")
     gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
 
     xml = 'data/haarcascades/haarcascade_frontalface_default.xml'
@@ -19,7 +21,7 @@ if __name__ == "__main__":
             cv2.rectangle(image1, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
 
-
+    print(time.time()-temp)
     plt.imshow(cv2.cvtColor(image1, cv2.COLOR_BGR2RGB), cmap='gray')
     plt.xticks([]), plt.yticks([])
     plt.show()
