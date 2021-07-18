ce1d424dc84df531d3472e0642bd9f9ecb8b1f8a
pinkishincoloragain <smb1103@gmail.com>
2021-07-18 20:32:32 +0900

    starting calculation

---
+++ detection/calculation.py
@@ -0,0 +1,18 @@
+
+# TODO
+# 인화성 물질
+# 고양이 멍멍이
+# 우선순위 - 위험도 (인간 가중치)
+# 물건 대비 위험도
+
+def flammable():
+    items = ['person','bicycle','firehydrant','bench','bird','cat','dog','cow','elephant','backpack','umbrella','handbag','tie','suitcase','frisbee','sportsball','kite','baseballbat','baseballglove','skateboard','tennisracket','bottle','wineglass','cup','fork','knife','spoon','bowl','banana','apple','sandwich','orange','broccoli','carrot','hotdog','pizza','donut','cake','chair','sofa','pottedplant','bed','diningtable','tvmonitor','laptop','mouse','remote','keyboard','cellphone','microwave','oven','toaster','sink','refrigerator','book','clock','vase','scissors','teddybear','hairdrier','toothbrush']
+
+
+def importance():
+
+
+if __name__ == "__main__":
+
+    res = 0
+    print(res)
