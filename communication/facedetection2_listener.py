8b510141b8b5df60731fe5b117ba9009b5e54c5f
choi-jaewon <sodlapdlf13@gmail.com>
2021-07-20 15:42:05 +0900

    add websocket to listener

--- communication/facedetection2_listener.py
+++ communication/facedetection2_listener.py
@@ -4,7 +4,8 @@ from cvlib.object_detection import draw_bbox
 import cv2
 import numpy as np
 from urllib.request import urlopen
-
+import websockets
+import asyncio
 
 # TODO
 # 인화성 물질
@@ -12,46 +13,58 @@ from urllib.request import urlopen
 # 우선순위 - 위험도 (인간 가중치)
 # 물건 대비 위험도
 
-url = "http://121.65.177.106:8091/?action=stream"
-stream = urlopen(url)
-buffer = b''
 
-# loop through frames
-while True:
-    buffer += stream.read(4096 * 4)
-    head = buffer.find(b'\xff\xd8')
-    end = buffer.find(b'\xff\xd9')
+# 비동기로 서버에 접속
+async def connect():
+    async with websockets.connect("ws://210.204.38.78:8080/python") as websocket:
+        url = "http://121.65.177.106:8091/?action=stream"
+        stream = urlopen(url)
+        buffer = b''
 
-    # read frame from stream
-    if head > -1 and end > -1:
-        jpg = buffer[head:end+2]
-        buffer = buffer[end+2:]
-        img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+        # loop through frames
+        while True:
+            buffer += stream.read(4096 * 4)
+            head = buffer.find(b'\xff\xd8')
+            end = buffer.find(b'\xff\xd9')
 
-        img = cv2.flip(img,1)
-    # if not status:
-    #     break
-    # 물체 검출
-        bbox, label, conf = cv.detect_common_objects(img)
+            # read frame from stream
+            if head > -1 and end > -1:
+                jpg = buffer[head:end + 2]
+                buffer = buffer[end + 2:]
+                img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
 
-        # print(bbox, label, conf)
-        # print(time.time())
+                img = cv2.flip(img, 1)
+                # if not status:
+                #     break
+                # 물체 검출
+                bbox, label, conf = cv.detect_common_objects(img)
 
-        # 검출된 물체 가장자리에 바운딩 박스 그리기
-        out = draw_bbox(img, bbox, label, conf, write_conf=True)
-        num = 0
+                # print(bbox, label, conf)
+                # print(time.time())
 
-        for item in label:
-            if item == "person":
-                num+=1
-        print(num)
+                # 검출된 물체 가장자리에 바운딩 박스 그리기
+                out = draw_bbox(img, bbox, label, conf, write_conf=True)
+                num = 0
 
-        # 디스플레이
-        cv2.imshow("Real-time object detection", out)
+                for item in label:
+                    if item == "person":
+                        num += 1
 
-    # Q 누르면 정지
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+                await websocket.send(str(num))
+
+                data = await websocket.recv()
+
+                print(data)
+
+                # 디스플레이
+                # cv2.imshow("Real-time object detection", out)
+
+            # Q 누르면 정지
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+asyncio.run(connect())
 
 # release resources
 cv2.destroyAllWindows()
